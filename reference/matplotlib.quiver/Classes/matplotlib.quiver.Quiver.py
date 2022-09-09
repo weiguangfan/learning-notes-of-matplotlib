6ddd2dc250a620fa265cfc2c41abbf01696dce77
weiguangfan <826008728@qq.com>
2022-09-09 18:35:24 +0800

    add files

---
+++ reference/matplotlib.quiver/Classes/matplotlib.quiver.Quiver.py
@@ -0,0 +1,22 @@
+"""
+class matplotlib.quiver.Quiver(
+                                ax,
+                                *args,
+                                scale=None,
+                                headwidth=3,
+                                headlength=5,
+                                headaxislength=4.5,
+                                minshaft=1,
+                                minlength=1,
+                                units='width',
+                                scale_units=None,
+                                angles='uv',
+                                width=None,
+                                color='k',
+                                pivot='tail',
+                                **kw)
+
+
+
+
+"""
